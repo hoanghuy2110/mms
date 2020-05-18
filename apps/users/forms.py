2c518ab5f0dfb89194d9c97861fac4939efb63da
khaicm-0897 <chu.minh.khai@sun-asterisk.com>
2020-05-18 09:13:01 +0700

    Fix bug screen profile

--- apps/users/forms.py
+++ apps/users/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 
-from app.users.models import (
+from apps.users.models import (
     UserPosition,
     UserProjectJoined,
     UserTeam,
--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from django.contrib.auth.decorators import login_required
 
-from app.users.views import (
+from .views import (
     UserPositionCreateView,
     UserPositionListView,
     UserPositionUpdateView,
